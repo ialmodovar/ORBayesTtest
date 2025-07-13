b966f6f6b556f964397f0ab6d46180839dfef8d6
Israel Almodovar-Rivera <israalmodovar@gmail.com>
2025-07-13 08:20:13 -0400

    Add files via upload

--- R/objective_and_robust_bayes_factors.R
+++ R/objective_and_robust_bayes_factors.R
@@ -96,17 +96,74 @@ two.sample.t.test.bf <- function(x,y,paired=FALSE,mu0,p0 = 0.5){
   }
 }
 
-ORBayesianTtest <- function(x,y = NULL, mu0 = 0, paired=FALSE,pH0 = 0.5){
-  if(is.null(y)){
-    res <- one.sample.t.test.bf(x = x, mu0 = mu0, p0 = pH0)
-  } else if(!is.null(y)){
-    res <- two.sample.t.test.bf(x = x, y = y,paired=paired,mu0 = mu0,p0 = pH0)
+ORBayesianTtest <- function(x, y = NULL, mu0 = 0, type="Robust", paired=FALSE, pH0 = 0.5)
+{
+  
+  if(pH0 < 0 & pH0 > 1){
+    stop("Error: P(H_0) needs to be between 0 and 1.\n")
   }
-  res
+  if(is.null(y)){
+    type <- match.arg(arg=type,choices = c("Intrinsic","Robust","Jeffreys"))
+  } else{
+    type <- match.arg(arg=type,choices = c("Intrinsic","Robust","Jeffreys","TESS","Schawrz"))
+  }
+  
+  if(is.null(y) | paired){
+    if(type!= "Robust"){
+      cat("\nOne sample objective Bayesian t-test\n")
+    } else{
+      cat("\nOne sample robust Bayesian t-test\n")
+    } 
+  } else{
+    if(type != "Robust"){
+      cat("\nTwo sample objective Bayesian t-test\n")
+    } else{
+      cat("\nTwo sample robust Bayesian t-test\n")
+    } 
+  }
+  if(is.null(y)){
+    cat("data: ", deparse(substitute(x)), "\n")  
+    cat(paste("mean of",deparse(substitute(x)),":",round(mean(x,na.rm=TRUE),digits=5),"\n"))
+    cat(paste("Prior probability of null hypothesis:",pH0 ,"\n"))
+    cat(paste("alternative hypothesis: true mean is not equal to",mu0 ,"\n"))
+    res <- one.sample.t.test.bf(x = x, mu0 = mu0, p0 = pH0)
+    oo <- round(res[type,],digits=5)
+    cat(paste("t = ", oo[,1],"; B01 = ",oo[,2],"; 2log(B01) = ",oo[,3],".\n")) 
+    pH0.data <- ifelse(oo[,4]< 0.0001,"< 0.0001",oo[,4])
+    cat("P(H0: mu =",mu0, "|", deparse(substitute(x)), ") = ",pH0.data,"\n")
+  } else{
+    if(!paired){
+      cat("data: ", deparse(substitute(x)), "and", deparse(substitute(y)), "\n")
+      cat(paste("mean of",deparse(substitute(x)),":",round(mean(x,na.rm=TRUE),digits=5),"\n"))
+      cat(paste("mean of",deparse(substitute(y)),":",round(mean(y,na.rm=TRUE),digits=5),"\n"))
+    } else{
+      d <- x-y
+      cat("data: ", deparse(substitute(x-y)), "\n")
+      cat(paste("mean of differences:",round(mean(d,na.rm=TRUE),digits=5),"\n"))
+    }
+    cat(paste("Prior probability of null hypothesis:",pH0 ,"\n"))
+    cat(paste("alternative hypothesis: true difference in means is not equal to",mu0 ,"\n"))
+    res <- two.sample.t.test.bf(x = x, y = y, paired = paired, mu0 = mu0, p0 = pH0)
+    oo <- round(res[type,],digits=5)
+    cat(paste("t = ", oo[,1],"; B01 = ",oo[,2],"; 2log(B01) = ",oo[,3],".\n")) 
+    pH0.data <- ifelse(oo[,4]< 0.0001,"< 0.0001",oo[,4])
+    if(!paired){
+      cat("P(H0: mu_1 - mu_2 =",mu0, "|", deparse(substitute(x)),",",deparse(substitute(y)), ") = ",pH0.data,"\n")
+    } else{
+      cat("P(H0: mu =",mu0, "|", deparse(substitute(x-y)),") = ",pH0.data,"\n")
+    }
+  }
+  
+  
+  invisible(list(tvalue = res[type,1],
+                 B01 = res[type,2],
+                 posterior = res[type,4]))
 }
 
+
+
 ##*****************
-##* Summary
+##* Bayes factors using summary statistics
 ##******************
 
 one.sample.t.test.bf.sm <- function(xbar,sx,n,mu0,p0 = 0.5){
@@ -179,12 +236,55 @@ two.sample.t.test.bf.sm <- function(xbar1,s21,n1,xbar2,s22,n2,mu0,p0 = 0.5)
   bf.all 
 }
 
-ORBayesianTtest.sm <- function(xbar1,s1,n1,xbar2 = NULL,s2 = NULL,n2 = NULL, mu0 = 0, pH0 = 0.5){
+ORBayesianTtest.sm <- function(xbar1, s1, n1, xbar2 = NULL, s2 = NULL, n2 = NULL,  mu0 = 0, type="Robust", pH0 = 0.5)
+{
   
-  if(is.null(xbar2) & is.null(s2) & is.null(n2) ){
-    res <- one.sample.t.test.bf.sm(xbar = xbar1, sx = s1, n = n1, mu0 = mu0, p0 = pH0)
-  } else if(!is.null(xbar2) & !is.null(s2) & !is.null(n2)){
-    res <- two.sample.t.test.bf.sm(xbar1 = xbar1, s21  = s1^2, n1 = n1,xbar2 = xbar2,s22 = s2^2,n2 = n2,mu0 = mu0,p0 = pH0)
+  if(pH0 < 0 & pH0 > 1){
+    stop("Error: P(H_0) needs to be between 0 and 1.\n")
   }
-  res
+  if(is.null(xbar2) & is.null(s2) & is.null(n2) )
+  {
+    type <- match.arg(arg=type,choices = c("Intrinsic","Robust","Jeffreys"))
+  } else{
+    type <- match.arg(arg=type,choices = c("Intrinsic","Robust","Jeffreys","TESS","Schawrz"))
+  }
+  
+  if((is.null(xbar2) & is.null(s2) & is.null(n2)) ){
+    
+    if(type!= "Robust"){
+      cat("\nOne sample objective Bayesian t-test\n")
+    } else{
+      cat("\nOne sample robust Bayesian t-test\n")
+    } 
+  } else{
+    if(type != "Robust"){
+      cat("\nTwo sample objective Bayesian t-test\n")
+    } else{
+      cat("\nTwo sample robust Bayesian t-test\n")
+    } 
+  }
+  if(is.null(xbar2) & is.null(s2) & is.null(n2) ){
+    
+    cat(paste("Prior probability of null hypothesis:",pH0 ,"\n"))
+    cat(paste("alternative hypothesis: true mean is not equal to",mu0 ,"\n"))
+    res <- one.sample.t.test.bf.sm(xbar = xbar1, sx = s1, n = n1, mu0 = mu0, p0 = pH0)
+    oo <- round(res[type,],digits=5)
+    cat(paste("t = ", oo[,1],"; B01 = ",oo[,2],"; 2log(B01) = ",oo[,3],".\n")) 
+    pH0.data <- ifelse(oo[,4]< 0.0001,"< 0.0001",oo[,4])
+    cat("P(H0: mu =",mu0, "|data) = ",pH0.data,"\n")
+  } else{
+    cat(paste("Prior probability of null hypothesis:",pH0 ,"\n"))
+    cat(paste("alternative hypothesis: true difference in means is not equal to",mu0 ,"\n"))
+    res <- two.sample.t.test.bf.sm(xbar1 = xbar1, s21  = s1^2, n1 = n1,xbar2 = xbar2,s22 = s2^2,n2 = n2,mu0 = mu0,p0 = pH0)
+    oo <- round(res[type,],digits=5)
+    cat(paste("t = ", oo[,1],"; B01 = ",oo[,2],"; 2log(B01) = ",oo[,3],".\n")) 
+    pH0.data <- ifelse(oo[,4]< 0.0001,"< 0.0001",oo[,4])
+    cat("P(H0: mu_1 - mu_2 =",mu0, "|data) = ",pH0.data,"\n")
+  }
+  
+  
+  invisible(list(tvalue = res[type,1],
+                 B01 = res[type,2],
+                 posterior = res[type,4]))
 }
+
